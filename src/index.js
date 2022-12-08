416612136bd2be119ed2e3417a3cad8acde39ce8
jeongbin <qls2467@gmail.com>
2022-12-08 16:32:21 -0700

    #2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 // 1) Import the React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 // 2) Get a reference to the dic with ID root
 const el = document.getElementById("root");
 // 3) Tell React to take control of that elememt
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-
-function App() {
-  let message = "Bye there";
-  if (Math.random() > 0.5) {
-    message = "Hello there";
-  }
-  return <h1>{message}</h1>;
-}
-
-// 5) Show the component on the screen
+// 4) Show the component on the screen
 root.render(<App></App>);
