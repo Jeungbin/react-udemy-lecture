b80d2c04f633be7c96cfb2cdc981c844d9be5cce
jeongbin <qls2467@gmail.com>
2022-12-10 09:03:30 -0700

    State Completed

--- src/StateChange/AnimalShow.js
+++ src/StateChange/AnimalShow.js
@@ -1,8 +1,31 @@
-export default function AnimalShow({ animalImg, heartImg }) {
+import { useState } from "react";
+import "./animalShow.css";
+import heart from "./images/heart.svg";
+import bird from "./images/bird.svg";
+import cat from "./images/cat.svg";
+import cow from "./images/cow.svg";
+import dog from "./images/dog.svg";
+import gator from "./images/gator.svg";
+import horse from "./images/horse.svg";
+
+const svgMap = {
+  // bird: bird
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+export default function AnimalShow({ type }) {
+  const [clickNum, setClickNum] = useState(0);
+  function handleClick() {
+    setClickNum(clickNum + 1);
+  }
   return (
-    <div>
-      <img style={{ width: "100px" }} src={animalImg} alt="" />
-      <img src={heartImg} alt="" />
+    <div onClick={handleClick}>
+      <img style={{ width: "10rem" }} src={svgMap[type]}></img>
+      <img style={{ width: 10 + 10 * clickNum + "px" }} src={heart}></img>
     </div>
   );
 }
--- src/StateChange/App.js
+++ src/StateChange/App.js
@@ -1,45 +1,33 @@
 import { useState } from "react";
 import AnimalShow from "./AnimalShow";
-import BirdImg from "./images/bird.svg";
-import CatImg from "./images/cat.svg";
-import CowImg from "./images/cow.svg";
-import DogImg from "./images/dog.svg";
-import GatorImg from "./images/gator.svg";
-import HeartImg from "./images/heart.svg";
-import HorseImg from "./images/horse.svg";
-const randomArray = [];
+import "./app.css";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  const animalArray = [BirdImg, CatImg, CowImg, DogImg, GatorImg, HorseImg];
-  const [num, setNum] = useState(0);
-  const handelPickNum = () => {
-    let randomNum = Math.floor(Math.random() * 6);
-    randomArray.push(randomNum);
-    setNum(randomNum);
-  };
+  const [animals, setAnimals] = useState([]);
+
+  function handleClick() {
+    //animals.push(getRandomAnimal()) => modifies a piece of state
+    setAnimals([...animals, getRandomAnimal()]);
+  }
+  console.log(animals);
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
 
   return (
-    <div>
-      <button onClick={handelPickNum}>Add Animal</button>
-      <div id="imageContainer" style={{ display: "flex" }}>
-        <AnimalShow
-          // heartImg= {randomArray.includes(0) ? }
-          animalImg={randomArray.includes(0) ? animalArray[0] : "none"}
-        />
-        <AnimalShow
-          animalImg={randomArray.includes(1) ? animalArray[1] : "none"}
-        />
-        <AnimalShow
-          animalImg={randomArray.includes(2) ? animalArray[2] : "none"}
-        />
-        <AnimalShow
-          animalImg={randomArray.includes(3) ? animalArray[3] : "none"}
-        />
-        <AnimalShow
-          animalImg={randomArray.includes(4) ? animalArray[4] : "none"}
-        />
-        <AnimalShow
-          animalImg={randomArray.includes(5) ? animalArray[5] : "none"}
-        />
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div
+        className="animal-list"
+        id="imageContainer"
+        style={{ display: "flex" }}
+      >
+        {renderAnimals}
       </div>
     </div>
   );
