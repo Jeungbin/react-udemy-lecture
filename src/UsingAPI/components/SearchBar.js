ac217724ebff239047679229dae562e67fe214a6
jeongbin <qls2467@gmail.com>
2022-12-10 23:14:37 -0700

    Handling Input Elements

--- src/UsingAPI/components/SearchBar.js
+++ src/UsingAPI/components/SearchBar.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setTerm(e.target.value);
+  };
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    onSubmit(document.getElementById("searchTxt").value);
+    // onSubmit(document.querySelector('input).value);
+    onSubmit();
   };
   return (
     <>
-      <form id="searchTxt" action="submit" onSubmit={handleFormSubmit}>
-        <input type="text" />
+      <form onSubmit={handleFormSubmit}>
+        <input onChange={handleChange} value={term} type="text" />
       </form>
     </>
   );
