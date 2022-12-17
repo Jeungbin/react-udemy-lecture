fd6f83ba1aa1fdf829d15baa842acee395cc011d
jeongbin <qls2467@gmail.com>
2022-12-16 17:59:24 -0700

    #9 : Using Icons in React Projects

--- src/10.Navigation and Routing/App.js
+++ src/10.Navigation and Routing/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Button from "./Button";
+import { GoBell } from "react-icons/go";
 export default function App() {
   return (
     <div>
       <div>
         <Button outline success primary>
+          <GoBell />
           click me!
         </Button>
       </div>
