f083512220e793429057177514dffec05e35590c
jeongbin <qls2467@gmail.com>
2022-12-17 19:52:08 -0700

    #11 : Inline Event Handlers

--- src/10.Navigation and Routing/components/Accordion.js
+++ src/10.Navigation and Routing/components/Accordion.js
@@ -3,16 +3,19 @@ import React, { useState } from "react";
 export default function Accordion({ items }) {
   const [expandedIndex, setEpandedIndex] = useState(0);
 
-  const handleClick = (id) => {
-    setEpandedIndex();
-  };
   const renderedItmes = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     const content = isExpanded && <div>{item.content}</div>;
     return (
       <div key={item.id}>
-        <div onClick={handleClick}>{item.label}</div>
+        <div
+          onClick={() => {
+            setEpandedIndex(index);
+          }}
+        >
+          {item.label}
+        </div>
         <div>{content}</div>
       </div>
     );
