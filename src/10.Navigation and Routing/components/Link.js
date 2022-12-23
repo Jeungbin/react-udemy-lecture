da1a7650288915902e59ebb60ea581f9bcbbb7ef
jeongbin <qls2467@gmail.com>
2022-12-23 00:24:18 -0700

    #13 : Handling Control and Command Keys

--- src/10.Navigation and Routing/components/Link.js
+++ src/10.Navigation and Routing/components/Link.js
@@ -1,16 +1,25 @@
 import { useContext } from "react";
+import classNames from "classnames";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
+  const classes = classNames("text-blue-500");
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
