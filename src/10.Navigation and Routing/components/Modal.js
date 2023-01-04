0dbe788b4eec241fe9429b28e367c9cc8b71324b
jeongbin <qls2467@gmail.com>
2023-01-04 16:23:56 -0700

    Customizing the Modal

--- src/10.Navigation and Routing/components/Modal.js
+++ src/10.Navigation and Routing/components/Modal.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-export default function Modal({ onClose }) {
+export default function Modal({ onClose, children, actionBar }) {
   return ReactDOM.createPortal(
     <div>
       <div
         onClick={onClose}
         className="absolute inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">I'm a modal</div>
+      <div className="absolute inset-40 p-10 bg-white">
+        {children}
+        {actionBar}
+      </div>
     </div>,
 
     document.querySelector(".modal-container")
--- src/10.Navigation and Routing/pages/ModalPage.js
+++ src/10.Navigation and Routing/pages/ModalPage.js
@@ -13,12 +13,24 @@ export default function ModalPage() {
     setShowModal(false);
   };
 
+  const actionBar = (
+    <div>
+      <Button primary>Acceapt</Button>
+    </div>
+  );
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is am inportant agreement for you</p>
+    </Modal>
+  );
+
   return (
     <div className="relative">
       <Button onClick={handleClick} primary>
         OPEN MODAL
       </Button>
-      {showModal && <Modal onClose={handleClose} />}
+      {showModal && modal}
       {/* <button onClick={handleClick}>Open Modal</button>
       {open ? <Modal onClick={handleClose} /> : ""} */}
     </div>
