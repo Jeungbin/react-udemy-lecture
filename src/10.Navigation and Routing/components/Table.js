df38360df50475a2834b8a72416b0ccf0026426b
jeongbin <qls2467@gmail.com>
2023-01-05 10:43:06 -0700

    #15 : Building the Rows

--- src/10.Navigation and Routing/components/Table.js
+++ src/10.Navigation and Routing/components/Table.js
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function Table({ data }) {
-  return <div>{data.length}</div>;
+  const renderdRows = data.map((item) => {
+    return (
+      <tr key={item.name}>
+        <td>{item.name}</td>
+        <td>
+          <div className=" h-10 w-10"></div>
+        </td>
+        <td>{item.score}</td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruits</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderdRows}</tbody>
+    </table>
+  );
 }
